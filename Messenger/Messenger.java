0e08a2445692d75bbd3d44dae4af0cec33f7c1a6
adrianm2430 <99854633+adrianm2430@users.noreply.github.com>
2022-03-23 20:09:19 +0700

    Add files via upload

---
+++ Messenger/Messenger.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Messenger;
+
+/**
+ *
+ * @author Rian
+ */
+public interface Messenger {
+    void sendMessage(String receiver, String subject, String messege);
+}
